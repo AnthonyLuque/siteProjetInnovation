09271e52d568b8af83c3a654a096e1d82f37e7f2
Unknown <anais.banos@gmail.com>
2018-01-05 20:01:50 +0100

    parametre co bd anais

--- site/functions/connexionBDD.php
+++ site/functions/connexionBDD.php
@@ -1,6 +1,6 @@
 	<?php
 		try{
-			$bdd = new PDO('mysql:host=localhost:8889;dbname=projetinnovant;charset=utf8', 'root', 'root');
+			$bdd = new PDO('mysql:host=localhost:3306;dbname=projetinnovant;charset=utf8', 'root', '');
 		}
 		catch (Exception $e){
 				die('Error : ' . $e->getMessage());
