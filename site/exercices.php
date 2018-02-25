7e0ccc136c45e12e4cf6e99ea28a243afec1093b
Anthony <anthony.luque@viacesi.fr>
2018-02-25 21:11:38 +0100

    Adding accordion on exercices page

--- site/exercices.php
+++ site/exercices.php
@@ -14,7 +14,59 @@
 		<link href="style/menu.css" rel="stylesheet" type="text/css" media="screen"> 
 		<link href="style/formulaires.css" rel="stylesheet" type="text/css" media="screen"> 
 		<link href="style/requetes.css" rel="stylesheet" type="text/css" media="screen">
+		
+		<style>
+			.accordion {
+				display: block;
+				background-color: #3cabbc;
+				color: white;
+				font-family: 'Open Sans', sans-serif;
+				cursor: pointer;
+				font-size:16px;
+				border: none;
+				outline: none;
+				
+				padding: 12px 26px;
+				text-decoration: none;
+				margin: 0px;
+				margin-bottom: 0;
+				margin-left: auto;
+				margin-right: auto;
+				width: 80%;
+				transition: 0.4s;
+			}
+
+			.active, .accordion:hover {
+				background-color: #308896;
+			}
+
+			.accordion:after {
+				content: '\002B';
+				color: white;
+				font-weight: bold;
+				float: right;
+				margin-left: 5px;
+			}
+
+			.active:after {
+				content: "\2212";
+			}
+
+			.panel {
+				display: block;
+				background-color: #ccc;
+				padding: 0 18px;
+				margin: 0;
+				margin-left: auto;
+				margin-right: auto;
+				max-height: 0;
+				width: 80%;
+				overflow: hidden;
+				transition: max-height 0.2s ease-out;
+			}
+		</style>
 	</head>
+	
 	
 	<?php 
 	// connexion à la bdd
@@ -57,11 +109,16 @@
 					
 					while($resultat = $req->fetch(PDO::FETCH_ASSOC)) {
 			?> 
-						<div class="resultatRequete"> 
+							<button class="accordion">
 			<?php
-							echo $resultat['nomExercice']; 
+								echo $resultat['nomExercice']; 
 			?> 
-						</div> 
+							</button>
+							<div class="panel">
+			<?php
+								echo "[Résultat Requête] Description de l'exercice."; 
+			?> 							
+							</div>
 			<?php
 					}
 				
@@ -69,6 +126,24 @@
 			?>
 			
 		</div>
+		
+
+		<script>
+			var acc = document.getElementsByClassName("accordion");
+			var i;
+
+			for (i = 0; i < acc.length; i++) {
+			  acc[i].addEventListener("click", function() {
+				this.classList.toggle("active");
+				var panel = this.nextElementSibling;
+				if (panel.style.maxHeight){
+				  panel.style.maxHeight = null;
+				} else {
+				  panel.style.maxHeight = panel.scrollHeight + "px";
+				} 
+			  });
+			}
+		</script>
 		
 
 		<!-- Footer -->
