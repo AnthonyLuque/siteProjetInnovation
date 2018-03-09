12cc65a602f431c1cf8672366b6925fed86836b8
Unknown <anthony.luque@viacesi.fr>
2018-03-10 00:00:28 +0100

    Adding bootstrap and other libraries

--- site/inscription - backup.php
+++ site/inscription - backup.php
@@ -10,10 +10,10 @@
 	<head>
 		<title> HandsRehab.com </title>
 		<meta charset = "UTF-8">
-		<link href="style/main.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="style/header.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="style/menu.css" rel="stylesheet" type="text/css" media="screen"> 
-		<link href="style/formulaires.css" rel="stylesheet" type="text/css" media="screen">
+		
+		<?php 
+			include ('functions/insertLibraries.php');
+		?>
 	</head>
 	
 	<?php 
--- site/statistiques.php
+++ site/statistiques.php
@@ -8,12 +8,10 @@
 	<head>
 		<title> HandsRehab.com </title>
 		<meta charset = "UTF-8">
-		<link href="style/main.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="style/header.css" rel="stylesheet" type="text/css" media="screen">
-		<link href="style/footer.css" rel="stylesheet" type="text/css" media="screen"> 
-		<link href="style/menu.css" rel="stylesheet" type="text/css" media="screen"> 
-		<link href="style/formulaires.css" rel="stylesheet" type="text/css" media="screen"> 
-		<link href="style/requetes.css" rel="stylesheet" type="text/css" media="screen">
+		
+		<?php 
+			include ('functions/insertLibraries.php');
+		?>
 	</head>
 	
 	<?php 
